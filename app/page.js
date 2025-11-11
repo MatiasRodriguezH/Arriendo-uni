2455be2f951fdd7969f0ceaa314fedc8484477d5
CoreFox20 <angeleduardosilvaarias@gmail.com>
2025-11-10 23:34:49 -0300

    avance en frontend pagina principal

--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
 import Header from "../components/Header";
+import Rentalview from "../components/Rentalview";
 import '../styles/home.css'
 
 export default function Home() {
   return (
-    <Header/> 
+    <div>
+      <Header/>
+        <div className="catalog">
+          <span style={{fontSize:'125%', fontWeight:'bold', marginBottom:'1%'}}>Principales Arriendos</span>
+          <Rentalview/>
+        </div>
+    </div>
   );
 }
---
+++ components/Rentalview.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+import "../styles/items.css"
+
+export default function Rentalview(){
+    return(
+        <div className="rentview-container">
+            <img src='./images/example.jpg'></img>
+            <div className="info">
+                <div className="type-property">
+                    <span style={{color:'white', margin:'0px 10px 0px 10px'}}>casa</span>
+                </div>
+                <span style={{textAlign:'start'}}>Titulo del arriendo</span>
+                <span style={{textAlign:'start', fontWeight:'bolder', fontSize:'1.2vw', marginBottom:'1%'}}>$999.999</span>
+                <span style={{textAlign:'start'}}>N Habitaciones | M baños</span>
+                <span style={{textAlign:'start', color:'grey'}}>Direccion</span>
+                <span style={{textAlign:'start', color:'#00638e'}}>Cercano a X ubicacion</span>
+            </div>
+        </div>
+    );
+}
