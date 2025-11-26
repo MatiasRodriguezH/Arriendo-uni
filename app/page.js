f7e331afefd5e6c17bce5ab59b400ba1910bb303
matias <ma.antoniorodriguez@gmail.com>
2025-11-26 16:27:58 -0300

    Dropdown menu

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import '../styles/home.css'
 
 export default function Home() {
   const [arriendos,setArriendos] = useState([]);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   useEffect(()=>{
     async function get_data() {
@@ -23,6 +24,35 @@ export default function Home() {
     get_data();
   },[]);
 
+  const handleRegionSelect = (regionNombre) => {
+    console.log(`Región seleccionada: ${regionNombre}`);
+    // Aquí puedes añadir la lógica para filtrar los arriendos
+    setIsDropdownOpen(false); // Cierra el dropdown después de la selección
+  }
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(prev => !prev);
+  }
+
+  const regiones = {
+    "Arica": 2,
+    "Maule": 1,
+    "Tarapacá": 3,
+    "Antofagasta": 4,
+    "Atacama": 5,
+    "Coquimbo": 6,
+    "Valparaíso": 7,
+    "Metropolitana": 8,
+    "Ñuble": 10,
+    "O'Higgins": 9, 
+    "Biobío": 11,
+    "La Araucanía": 12,
+    "Los Rios": 13,
+    "Los Lagos": 14,
+    "Aysén": 15,
+    "Magallanes": 16
+  }
+
   return (
     <div className="page">
       <Header/>
@@ -30,10 +60,31 @@ export default function Home() {
         <div className="uni-search">
           <h1 className="search-titulo">Busca según tu universidad</h1>
           <div className="uni-inputs">
-          <button className="region-search">
-            Region
-          </button>
-          <input className="universidad" type="text" name="universidad" placeholder="Universidad Católica del Maule"></input>
+            <div className="button-div">
+              {/* 2. Asignamos la función toggleDropdown al click del botón principal */}
+              <button className="region-search" type="button" onClick={toggleDropdown}>
+                Region
+              </button>
+              {/* 3. Renderizado Condicional: El div solo se muestra si isDropdownOpen es true */}
+              {isDropdownOpen && (
+                <div className="region-dropdown" name="dropdown">
+                  <ul>
+                  {Object.keys(regiones).map((regionNombre) => (
+                    <li key={regionNombre}>
+                      {/* 4. Usamos handleRegionSelect en los botones del dropdown */}
+                      <button 
+                        className="dropdown-item" 
+                        onClick={() => handleRegionSelect(regionNombre)}
+                      >
+                        {regionNombre}
+                      </button>
+                    </li>
+                  ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+            <input className="universidad" type="text" name="universidad" placeholder="Universidad Católica del Maule"></input>
           </div>
         </div>
         <div className="search-page">
